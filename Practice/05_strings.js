ac7249e630e95792d7574c078cd8bc1a7bad5cdc
ravisingh9990 <89929744+ravisingh9990@users.noreply.github.com>
2023-09-23 10:40:29 +0000

    String methods practiced in detail

--- Practice/05_strings.js
+++ Practice/05_strings.js
@@ -70,3 +70,74 @@ console.log(url.includes('ravi'));//Output: true
 const newStrStr = "this-is-an-apple";
 
 console.log(newStrStr.split("-"));
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let tagline = "Buy a company, not Products";
+
+console.log(tagline.length); //27
+console.log(tagline.charCodeAt(6));//99
+console.log(tagline.substring(4, tagline.length-2));//a company, not Produc
+console.log(tagline.replace("Buy", "Purchase"));//Purchase a company, not Products
+console.log(tagline.substr(2, 24));//y a company, not Product
+console.log(tagline.toLowerCase());//buy a company, not products
+console.log(tagline.toUpperCase());//BUY A COMPANY, NOT PRODUCTS
+console.log(tagline.toLocaleLowerCase());//buy a company, not products
+console.log(tagline.toLocaleUpperCase());//BUY A COMPANY, NOT PRODUCTS
+
+
+
+let taglineTrim = "   Buy a company, not Products          ";
+console.log(taglineTrim.trim());//Buy a company, not Products
+
+
+let taglineExtraTrim = "   B     u     y a company, not Products          ";
+//This will trim the spaces only from start and end of the string
+console.log(taglineExtraTrim.trim());//B     u     y a company, not Products.
+
+console.log(taglineTrim.trimEnd());//   Buy a company, not Products
+
+console.log(taglineTrim.trimStart());//Buy a company, not Products          
+
+console.log(taglineTrim.trimLeft());//Buy a company, not Products          
+
+
+console.log(taglineTrim.trimRight());//   Buy a company, not Products
+
+console.log(tagline.includes("hello"));//false
+console.log(tagline.includes("company"));//true
+console.log(tagline.charAt(8));//m
+
+
+console.log(tagline.charAt(8,10, 12));//m //Because it only accepts only one argument
+
+
+console.log(tagline.concat(":Stockpedia"));//Buy a company, not Products:Stockpedia
+console.log(tagline.startsWith("B"));//true
+console.log(tagline.endsWith("s"));//true
+console.log(tagline.indexOf("s"));//26 //length is 27 but the index of last character is 26 because of zero based indexing 
+console.log(tagline.repeat(2));//Buy a company, not ProductsBuy a company, not Products
+
+let taglineExtend = "Buy a company, not Products of a company";
+console.log(taglineExtend.length());//40
+console.log(taglineExtend.lastIndexOf("y"))//39
+
+console.log(taglineExtend.match(/[A-Z]/g));//B , P
+
+
+//Note: For using the padEnd 1st argument should be greater than the length of your string.
+console.log(tagline.padEnd(30, "?"));//Buy a company, not Products???
+
+console.log(tagline.padStart(30, "?"));//???Buy a company, not Products
+
+
+console.log(tagline.search("o"));//7 //Returns the index number of the character or word
+console.log(tagline.slice(4,13));//a company
+console.log(tagline.split(""));//[ "B", "u", "y", " ", "a", " ", "c", "o", "m", "p", … ] //It breaks down the string
+
+
+
+
+
